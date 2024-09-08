5dfcad00821e3e37e9758c6bab7d6022799f660b
abduljabbar1010 <abduuljabbar1010@gmail.com>
2024-09-08 21:54:19 +0700

    fix1

--- app/(tabs)/experience.tsx
+++ app/(tabs)/experience.tsx
@@ -4,7 +4,7 @@ import {View, Text } from "react-native"
 export default function Experience() {
     return (
         <View>
-         <Text>apa yaa</Text>
+         <Text>ini halaman Experience</Text>
         </View>
     )
 }
