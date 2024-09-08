673f622fc386c89c95f3ce38a6d7acc145a63da5
abduljabbar1010 <abduuljabbar1010@gmail.com>
2024-09-08 21:31:13 +0700

    coba perubahan git

--- app/(tabs)/experience.tsx
+++ app/(tabs)/experience.tsx
@@ -4,7 +4,7 @@ import {View, Text } from "react-native"
 export default function Experience() {
     return (
         <View>
-         <Text>ini halaman Experience </Text>
+         <Text>apa yaa</Text>
         </View>
     )
 }
